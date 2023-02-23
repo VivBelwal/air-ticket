d4eb154c859021c17853dd9b596d386d758cca11
VivBelwal <04vivek10belwal.com>
2023-02-23 13:30:56 +0530

    working on the book routes

--- src/routes/flights.route.js
+++ src/routes/flights.route.js
@@ -16,20 +16,74 @@ FlightRoute.post("/flights", async (req,res) =>{
        
     }
 })
+FlightRoute.get("/flights/:id", async (req,res) =>{
+    let id = req.params.id;
 
-FlightRoute.get("/flights", async (req,res) =>{
-    
-
+    console.log(id)
     try{
-
-        // let flights = await Flight.
+ 
+        let flights = await Flight.findOne({_id: id});
+        console.log(flights);
           
-            return  res.status(201).send({status : "Success", message :  "Flight Added successfully"})
+            return  res.status(200).send({status : "Success", data : flights})
      
     }catch(e){
         return  res.status(400).send({status : "Falied", message :  "Bad Request"})
        
     }
 })
+FlightRoute.patch("/flights/:id", async (req,res) =>{
+    let id = req.params.id;
+       let {airline,flightNo,departure,arrival,departureTime,arrivalTime,seats,price} = req.body;
+
+    
+    try{
+ 
+       await Flight.updateOne({_id : id}, {$set: {airline,flightNo,departure,arrival,departureTime,arrivalTime,seats,price}})
+       
+       
+            return  res.status(204).send({status : "Success",message :  "Flights Update successfuly", data : flights})
+     
+    }catch(e){
+        return  res.status(400).send({status : "Falied", message :  "Bad Request"})
+       
+    }
+})
+FlightRoute.delete("/flights/:id", async (req,res) =>{
+    let id = req.params.id;
+       
+
+    
+    try{
+ let flights = await Flight.deleteOne({_id: id})
+       
+       
+       
+            return  res.status(202).send({status : "Success",message :  "Flight Deleted successfuly", data : flights})
+     
+    }catch(e){
+        return  res.status(400).send({status : "Falied", message :  "Bad Request"})
+       
+    }
+})
+
+
+// 
+FlightRoute.get("/flights", async (req,res) =>{
+    
+
+    try{
+
+        let flights = await Flight.find({});
+        console.log(flights);
+          
+            return  res.status(200).send({status : "Success", message :  "All available Flights" , data : flights})
+     
+    }catch(e){
+        return  res.status(400).send({status : "Falied", message :  "Bad Request"})
+       
+    }
+})
+
 
 module.exports = FlightRoute
